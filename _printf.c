882b658b91f73fc8fa2ff316701c55a86a84f157
bothainakb <bothainakb@gmail.com>
2026-04-15 08:47:19 +0000

    Fix edge case for % at end of format string

--- _printf.c
+++ _printf.c
@@ -8,38 +8,41 @@
  */
 int _printf(const char *format, ...)
 {
-    int i, count = 0;
-    va_list args;
+	int i, count = 0;
+	va_list args;
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
 
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] == '%')
-        {
-            i++;
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+				return (-1);
 
-            if (format[i] == 'c')
-                count += print_char(va_arg(args, int));
-            else if (format[i] == 's')
-                count += print_string(va_arg(args, char *));
-            else if (format[i] == '%')
-                count += write(1, "%", 1);
-            else
-            {
-                count += write(1, "%", 1);
-                count += write(1, &format[i], 1);
-            }
-        }
-        else
-        {
-            count += write(1, &format[i], 1);
-        }
-    }
+			i++;
 
-    va_end(args);
-    return (count);
+			if (format[i] == 'c')
+				count += print_char(va_arg(args, int));
+			else if (format[i] == 's')
+				count += print_string(va_arg(args, char *));
+			else if (format[i] == '%')
+				count += write(1, "%", 1);
+			else
+			{
+				count += write(1, "%", 1);
+				count += write(1, &format[i], 1);
+			}
+		}
+		else
+		{
+			count += write(1, &format[i], 1);
+		}
+	}
+
+	va_end(args);
+	return (count);
 }
